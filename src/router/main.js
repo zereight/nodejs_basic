2355eeab0b6ba26732af14d47065f1afd6224899
zereight <zereight@zereightui-MacBook.local>
2020-07-30 13:42:29 +0900

    ejs 사용하기

--- src/router/main.js
+++ src/router/main.js
@@ -1,9 +1,8 @@
 module.exports = function (app) {
   app.get("/", function (req, res) {
-    res.render("index.html");
-  });
-
-  app.get("/about", function (req, res) {
-    res.render("about.html");
+    res.render("index", {
+      title: "my homepage",
+      length: 5,
+    });
   });
 };
--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const fs = require("fs");
 
 const app = express();
 
@@ -7,6 +10,15 @@ app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "sajiusdhfuiwehui", // 쿠키를 임으로 변경하는 것을 막기위한 sign값
+    resave: false, // 세션을 언제나 저장할 것인지
+    saveUninitialized: true, // 변경되지않은 세션을 저장할 것인지
+  })
+);
 
 app.listen(3000, () => {
   console.log("Hi");
